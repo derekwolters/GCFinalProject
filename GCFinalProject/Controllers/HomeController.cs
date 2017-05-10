f786774e347940b0db2fe291d4b739d598d349ac
Bruce Lack <bruce.a.lack@gmail.com>
2017-05-10 09:30:31 -0400

    Scaffolded out models based on the Database

--- GCFinalProject/Controllers/HomeController.cs
+++ GCFinalProject/Controllers/HomeController.cs
@@ -7,6 +7,11 @@ using System.Net.Http;
 using System.Net;
 using System.IO;
 using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Xml;
 
 namespace GCFinalProject.Controllers
 {
@@ -15,7 +20,6 @@ namespace GCFinalProject.Controllers
     {
         const string clientID = "4e30bc62";
         const string clientKey = "ec86a3101ba85f41ca22c992867e8d10";
-
         public ActionResult Index()
         {
             return View();
@@ -34,18 +38,27 @@ namespace GCFinalProject.Controllers
 
             return View();
         }
+        public ActionResult Craving()
+        {
+            return View();
+        }
+        public ActionResult Success()
+        {
+            ViewBag.Message = "Sorry to hear that you're craving sweets. This could stem from a deficiency in: ";
+            return View();
+        }
 
         public ActionResult Results()
         {
-            //var url = "https://api.edamam.com";
+            //var URL = "https://api.edamam.com";
             //var strPostData = "https://api.edamam.com/search?q=" + searchTerm;
             //strPostData += "&app_id=" + clientID;
-            //strPostData += "&app_key=" + clientKey;     
+            //strPostData += "&app_key=" + clientKey;
             ////NOvar strPostData = "https://api.edamam.com/search?q=chicken&app_id=4e30bc62&app_key=ec86a3101ba85f41ca22c992867e8d10&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free";
-            //WebClient wc = new WebClient();
+            //WebClient WC = new WebClient();
 
             //ViewBag.testUrl = strPostData;
-            //var strResponse = wc.UploadString(url, strPostData);
+            //var strResponse = wc.UploadString(URL, strPostData);
             ////NOvar login = JavascriptDeserialize<OAuthLogin>(strResponse);
             ////NOvar strSearch = "https://api.edamam.com/search";
             ////NOwc.Headers.Add("Authorization", "Bearer " + login.access_token);
@@ -96,5 +109,5 @@ namespace GCFinalProject.Controllers
 
             return View();
         }
-    }        
+    }
 }
